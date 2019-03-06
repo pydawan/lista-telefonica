5f5dae103598d6b4639b0333a5ec151c720d6a57
Thiago Alexandre <thicmp@gmail.com>
2019-03-06 17:56:05 -0300

    Atualizado o projeto.

--- js/config/routeConfig.js
+++ js/config/routeConfig.js
@@ -1,6 +1,6 @@
 angular.module("listaTelefonica").config(function($routeProvider) {
-    $routeProvider.when("/contatos", {
-        templateUrl: "view/contatos.html",
-        controller: "listaTelefonicaCtrl"
-    });
+  $routeProvider.when("/contatos", {
+    templateUrl: "view/contatos.html",
+    controller: "listaTelefonicaCtrl"
+  });
 });
--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -1,4 +1,4 @@
-angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($scope, contatosAPI, operadorasAPI, serialGenerator, $filter, uppercaseFilter) {
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, contatosAPI, operadorasAPI, serialGenerator, $filter, uppercaseFilter) {
     $scope.app = "Lista Telefônica";
     /*
     contatosAPI.getContatos()
@@ -10,36 +10,36 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($sc
     });
     */
     $scope.contatos = [
-        {nome: $filter("uppercase")("Pedro"), telefone: "99998888", cor: "blue", data: new Date(), serial: serialGenerator.generate()},
-        {nome: $filter("uppercase")("Ana"), telefone: "99998877", cor: "yellow", data: new Date(), serial: serialGenerator.generate()},
-        {nome: uppercaseFilter("Maria"), telefone: "99998866", cor: "red", data: new Date(), serial: serialGenerator.generate()}
+        { nome: $filter("uppercase")("Pedro"), telefone: "99998888", cor: "blue", data: new Date(), serial: serialGenerator.generate() },
+        { nome: $filter("uppercase")("Ana"), telefone: "99998877", cor: "yellow", data: new Date(), serial: serialGenerator.generate() },
+        { nome: uppercaseFilter("Maria"), telefone: "99998866", cor: "red", data: new Date(), serial: serialGenerator.generate() }
     ];
     $scope.operadoras = [
-        {nome: "Oi", codigo: 14, categoria: "Celular", preco: 2},
-        {nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1},
-        {nome: "Tim", codigo: 41, categoria: "Celular", preco: 3},
-        {nome: "GVT", codigo: 25, categoria: "Fixo", preco: 1},
-        {nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2},
+        { nome: "Oi", codigo: 14, categoria: "Celular", preco: 2 },
+        { nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1 },
+        { nome: "Tim", codigo: 41, categoria: "Celular", preco: 3 },
+        { nome: "GVT", codigo: 25, categoria: "Fixo", preco: 1 },
+        { nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2 },
     ];
     $scope.classe1 = "selecionado";
     $scope.classe2 = "negrito";
-    $scope.adicionarContato = function(contato) {
+    $scope.adicionarContato = function (contato) {
         contato.serial = serialGenerator.generate();
         $scope.contatos.push(angular.copy(contato));
         delete $scope.contato;
         $scope.contatoForm.$setPristine();
     };
-    $scope.apagarContatos = function(contatos) {
-        $scope.contatos = contatos.filter(function(contato) {
+    $scope.apagarContatos = function (contatos) {
+        $scope.contatos = contatos.filter(function (contato) {
             if (!contato.selecionado) return contato;
         });
     };
-    $scope.isContatoSelecionado = function(contatos) {
-        return contatos.some(function(contato) {
+    $scope.isContatoSelecionado = function (contatos) {
+        return contatos.some(function (contato) {
             return contato.selecionado;
         });
     };
-    $scope.ordenarPor = function(campo) {
+    $scope.ordenarPor = function (campo) {
         $scope.criterioDeOrdenacao = campo;
         $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
     };
